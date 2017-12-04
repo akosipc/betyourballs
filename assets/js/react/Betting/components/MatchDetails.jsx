63848a214499e40b6e09cd4b0f01528421a1bb46
Adrian Peterson Co <adrianpco@gmail.com>
2017-12-04 17:59:18 +0800

    Adds the necessary containers and components
    - BettingContainer
    - BettingProgress
    - MatchDetails

---
+++ assets/js/react/Betting/components/MatchDetails.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class MatchDetails extends Component {
+  render () {
+    return (
+      <div>
+        MatchDetails
+      </div>
+    )
+  }
+}
+
+MatchDetails.propTypes = {
+}
--- assets/js/react/Betting/containers/BettingContainer.jsx
+++ assets/js/react/Betting/containers/BettingContainer.jsx
@@ -1,16 +1,22 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import MatchDetails from '../components/MatchDetails'
+import BettingProgress from '../components/BettingProgress'
 
 export default class BettingContainer extends Component {
   render () {
     return (
       <div>
-        Tits
+        <MatchDetails/>
+        <BettingProgress/>
       </div>
     )
   }
 }
 
 BettingContainer.propTypes = {
-
+  match: PropTypes.object.isRequired,
+  betProgress: PropTypes.object.isRequired
 }
 

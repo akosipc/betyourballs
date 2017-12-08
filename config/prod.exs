0847113a93de989d75f4913cfcdefecc92bffee4
Adrian Peterson Co <adrianpco@gmail.com>
2017-12-08 15:31:41 +0800

    Whoops! I forgot the comma

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :bet_your_balls, BetYourBallsWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "betyourballs.herokuapp.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
